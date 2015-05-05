e16b479ea855176cb0767c2ddb75ec5f39ab6615
mambax7 <mambax7@gmail.com>
2015-05-05 09:30:55 -0400

    Original files

---
+++ solid_silver/txc_themeinfo.php
@@ -0,0 +1,7 @@
+<?php
+	$txc_themeinfo["name"] = "solid_silver";
+	$txc_themeinfo["author"] = "BCOOL";
+	$txc_themeinfo["author_url"] = "http://2bcool.net";
+	$txc_themeinfo["detail_url"] = "hhttp://2bcool.net/mydownloads+index.htm";
+	$txc_themeinfo["download_url"] = "http://2bcool.net/mydownloads+index.htm";
+?>
